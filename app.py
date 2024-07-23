538a529da45a6c4c804148e1b25a7e8d94821d6a
ujikol101 <2047287902@qq.com>
2024-07-23 16:27:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 from flask import Flask,render_template,request
+import google.generativeai as palm
+
+palm.configure(api_key="AIzaSyDyFBJKUtHaoz2vF1tQBz_PJgx_2kbadhw")#google api设置
+model = {"model":"models/chat-bison-001"} #model选择
+
+
 app = Flask(__name__)
+
+
 @app.route("/",methods=["GET","POST"])
 def index():
     return(render_template("index.html"))
@@ -8,5 +16,11 @@ def index():
 def main():
     r = request.form.get("q")
     return(render_template("main.html",r=r))
+@app.route("/genAI",methods=["GET","POST"])
+def genAI():
+    q = request.form.get("q")
+    r = palm.chat(**model,messages=q)
+    
+    return(render_template("genAI.html",r=r.last))
 if __name__ == "__main__":
     app.run()
